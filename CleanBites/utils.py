eebf79ffe2119060c31b3303a7220e0c7f79c8fd
Jefferyl240 <Jefferyl240@gmail.com>
2025-03-17 22:23:26 -0400

    UI improvement

--- CleanBites/utils.py
+++ CleanBites/utils.py
@@ -128,8 +128,10 @@ def create_nyc_map(features):
                     </div>
                 </div>
                 """
-                iframe = folium.IFrame(html=popup_html, width=260, height=140)
-                popup = folium.Popup(iframe, max_width=300)
+                iframe_height = 170 if properties.get("address") else 150
+                iframe = folium.IFrame(html=popup_html, width=270, height=iframe_height)
+                popup = folium.Popup(iframe, max_width="auto")
+                
                 color = get_color(properties.get("rating", -1))
                 folium.Marker(
                     location=[lat, lon],
