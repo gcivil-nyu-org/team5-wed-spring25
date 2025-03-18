257a8244a9a8c1634ee15d2dea69f2cab2c3e107
jwb10028 <143141491+jwb10028@users.noreply.github.com>
2025-03-18 16:55:40 -0400

    add authentication error codes

--- CleanBites/_frontend/views.py
+++ CleanBites/_frontend/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from _api._users.models import Customer
+from django.contrib import messages
 
 User = get_user_model()
 
