63b9c9b3010c3e3acd3a45e2feef3c7aa32f2ca5
jwb10028 <143141491+jwb10028@users.noreply.github.com>
2025-04-11 22:02:10 -0400

    add global search

--- CleanBites/_frontend/urls.py
+++ CleanBites/_frontend/urls.py
@@ -51,5 +51,5 @@ urlpatterns = [
         "delete_comment/<int:comment_id>/", views.delete_comment, name="delete_comment"
     ),
     path("profileedit/", views.update_profile, name="update_profile"),
-    path("bookmarks/", views.bookmarks_view, name="bookmarks_view"),
+    path("global-search/", views.global_search, name="global_search"),
 ]
