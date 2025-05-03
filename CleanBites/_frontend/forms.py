03e411d6855af8d0f46d95aa3268d0ff865ce411
Jefferyl240 <Jefferyl240@gmail.com>
2025-05-03 19:55:23 -0400

    removing user access to change email/username, which cause problems

--- CleanBites/_frontend/forms.py
+++ CleanBites/_frontend/forms.py
@@ -1,8 +1,7 @@
 from django.forms import ModelForm, Textarea
 from django import forms
 from _api._restaurants.models import Comment
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.models import User
+
 
 
 class Review(ModelForm):
@@ -12,12 +11,6 @@ class Review(ModelForm):
         widgets = {
             "comment": Textarea(attrs={"cols": 80, "rows": 8}),
         }
-
-
-class EmailChangeForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["email"]
 
 
 class DeactivateAccountForm(forms.Form):
